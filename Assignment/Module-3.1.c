d59ce7ac4ad3f64248d09770a00461d9ea69650e
Nikul Parmar <118187823+Nick1595@users.noreply.github.com>
2022-12-07 14:51:40 +0530

    Update and rename Module-3.1 to Module-3.1.c

---
+++ Assignment/Module-3.1.c
@@ -0,0 +1,50 @@
+                                                                     Q - 1
+
+/* WAP Display This Information using printf 
+1. Your Name 
+2. Your Birth date
+3. Your Age 
+4. Your Address*/
+
+#include<stdio.h>
+
+void main()
+{
+	printf("Name : Nikul \n");
+	printf("DOB : 01/07/2004 \n");
+	printf("AGE : 19");
+    printf("Address : 8th floor,808 bhurj khalifa , Dubai");
+		
+	
+	
+}
+
+
+                                                              Q - 2
+							      
+							       
+							       
+#include<stdio.h>
+void main ()
+{
+	int A ;
+	int B ;
+	
+	printf("Enter the value of A=");
+	scanf("%d",&A);
+	printf("Enter the value of B=");
+	scanf("%d",&B);
+	
+	
+	printf("A + B = %d \n",A+B);
+	printf("A - B = %d \n",A-B);
+    printf("A * B = %d \n",A*B);
+	printf("A / B = %d \n",A/B);
+    printf("A % B = %d \n",A%B);	
+}
+                                                        Q - 3	
+							
+							
+							
+							
+							
