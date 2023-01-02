5a2d9ef591c6188e3816ec9d95329b7e5a646103
Nikul Parmar <118187823+Nick1595@users.noreply.github.com>
2023-01-02 15:50:39 +0530

    Create Odd-Even ternary.c

---
+++ Assignment/Module-3/Module-3.2/Odd-Even ternary.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int no;
+ 	
+	printf("Enter the number = ");
+	scanf("%d",&no);
+
+    (no%2==0) ? (printf("Your no is even") ): (printf("Your no is odd"));
+}
+	
+	
