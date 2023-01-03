843983905d5d2dd912cf9ab575b9bba91c086bf2
Nikul Parmar <118187823+Nick1595@users.noreply.github.com>
2023-01-03 17:55:07 +0530

    Update looping.c

--- Assignment/Module-3/Module-3.2/looping.c
+++ Assignment/Module-3/Module-3.2/looping.c
@@ -38,3 +38,34 @@ void main()
 	printf("No of even is %d\n",even);
 	printf("No of odd is %d\n",odd);
 }
+
+
+3.#include<stdio.h>
+void main()
+{
+	int i,odd=0,even=0, n[10],osum=0,esum=0;
+		
+	for( i=0; i<10; i++)
+	{
+	printf("Enter number: ");
+	scanf("%d",&n[i]);
+	
+
+		if(n[i]%2==0) 
+		{
+			even++;
+			esum += n[i];	
+		}
+		else
+		{
+			odd++;
+			osum += n[i];
+		}
+		
+	}
+	printf("\n------------------------\n");
+	printf("No of even is %d\n",even);
+	printf("No of odd is %d\n",odd);
+	printf("sum of even is %d\n",esum);
+	printf("sum of odd is %d\n",osum);
+}
