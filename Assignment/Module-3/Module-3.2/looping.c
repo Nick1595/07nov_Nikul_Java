1cce5d532eeb491139ddbe83ab3fb6b891e8337b
Nikul Parmar <118187823+Nick1595@users.noreply.github.com>
2023-01-03 17:29:01 +0530

    Update looping.c

--- Assignment/Module-3/Module-3.2/looping.c
+++ Assignment/Module-3/Module-3.2/looping.c
@@ -10,3 +10,31 @@ void main()
 }
 
 2.
+ #include<stdio.h>
+void main()
+{
+	int i,odd=0,even=0, n[10];
+		
+	for( i=0; i<10; i++)
+	{
+	printf("Enter number: ");
+	scanf("%d",&n[i]);
+	
+
+		if(n[i]%2==0) 
+		{
+			even++;
+			
+			
+		}
+		else
+		{
+			odd++;
+			
+			
+		}
+		
+	}
+	printf("No of even is %d\n",even);
+	printf("No of odd is %d\n",odd);
+}
